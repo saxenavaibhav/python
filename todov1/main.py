a9b9e6c434fca6fc166a71a0e82a209630e437fd
Vaibhav Saxena <reachvaibhavsaxena@gmail.com>
2023-09-16 09:39:27 +0530

    Push

--- todov1/main.py
+++ todov1/main.py
@@ -1,10 +1,23 @@
-#print("Enter a todo: ")
-user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(type(todo1))
-print(type(todos))
-print(todos)
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit or exit: ")
+    match user_action.strip():
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show | display':
+            for item in todos:
+                print(item)
+        case 'edit':
+            index = input("Enter index of item to be edited: ")
+            new_todo = input("Enter new item: ")
+            todos[int(index)-1] = new_todo
+            print(todos)
+        case 'exit':
+            break
+        case default:
+            print("You entered an incorrect command")
+print('Bye')
+
